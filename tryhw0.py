735564f0d0802d2c7d8464d2eccd30725e488c32
Logan Milandin <mi1andin@uw.edu>
2021-10-17 21:34:35 -0700

    answers questions in tryhw0.py

--- tryhw0.py
+++ tryhw0.py
@@ -44,3 +44,15 @@ print
 print("evaluating model...")
 print("training accuracy: %f", accuracy_net(m, train))
 print("test accuracy:     %f", accuracy_net(m, test))
+
+# We reached about .975 accuracy on MNIST data.
+
+# Our accuracy on CIFAR is much lower than on mnist. The best test accuracy
+# we could reach is around .51.
+
+# We found that the same parameters for rate, momentum, iters, and decay 
+# worked well for both problem spaces. However, we found that making just one
+# layer with many neurons improved accuracy a lot for MNIST, whereas adding
+# more layers improved accuracy more on CIFAR. We think this is because the CIFAR
+# problem space is more difficult, so the model benefits more from additional
+# complexity from the extra activation layers.
